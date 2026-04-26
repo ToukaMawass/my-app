452c67290800aa67d2b9e18c92f9505d08f7e891
touka mawass <toukamawasss7@gmail.com>
2026-04-26 17:02:42 -0400

    updated UI with navbar button and slider

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,26 @@
+import Slider from '@mui/material/Slider';
+
 function App() {
   return (
     <div>
+      {/* Navbar */}
       <nav className="navbar navbar-expand-lg navbar-light bg-light">
         <div className="container-fluid">
-          <a className="navbar-brand" href="#">My App</a>
-          <button className="navbar-toggler" type="button">
-            <span className="navbar-toggler-icon"></span>
-          </button>
+          <a className="navbar-brand" href="#">
+            My App
+          </a>
         </div>
       </nav>
 
+      {/* Content */}
       <div style={{ padding: "20px" }}>
-        <button>Click Me</button>
+        <button className="btn btn-primary">
+          Click Me
+        </button>
+
+        <div style={{ width: 300, marginTop: 20 }}>
+          <Slider defaultValue={50} />
+        </div>
       </div>
     </div>
   );
