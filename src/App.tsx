a51cbb6a6e73b846bd43b342e62ca224956798ef
touka mawass <toukamawasss7@gmail.com>
2026-04-26 16:50:55 -0400

    added bootstrap navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
 function App() {
   return (
     <div>
-      <button>Click Me</button>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#">My App</a>
+          <button className="navbar-toggler" type="button">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+        </div>
+      </nav>
+
+      <div style={{ padding: "20px" }}>
+        <button>Click Me</button>
+      </div>
     </div>
   );
 }
